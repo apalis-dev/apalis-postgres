eb76a8b3830283b9161df9ffc8782ef2e35914cf
Justus K <justus.k@protonmail.com>
2025-12-22 20:59:32 +0100

    fix: allow sqlx `chrono` and `time` features to be enabled

--- queries/task/fetch_next.sql
+++ queries/task/fetch_next.sql
@@ -1,4 +1,16 @@
 SELECT
-    *
+    job,
+    id,
+    job_type,
+    status,
+    attempts,
+    max_attempts,
+    run_at as "run_at: chrono::DateTime<chrono::Utc>",
+    last_result,
+    lock_at as "lock_at: chrono::DateTime<chrono::Utc>",
+    lock_by,
+    done_at as "done_at: chrono::DateTime<chrono::Utc>",
+    priority,
+    metadata
 FROM
     apalis.get_jobs($1, $2, $3)
